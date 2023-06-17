a970c56d79ce158ef129dccb856f1a7c9d77d862
fkurushin <f.kurushin@ya.ru>
2023-06-17 14:29:29 +0300

    edit condition

--- regulator.py
+++ regulator.py
@@ -1,15 +1,14 @@
-import datetime
-
 class DifferentialRegulator:
-    COEFFICIENT = 0.1
-
-    def __init__(self, time0=0, temp0=0, time1=None, temp1=None):
+    COEFFICIENT = 10
+    def __init__(self, temp_max, temp_min, time0=0, temp0=0, time1=None, temp1=None):
         """
         :param time0: time of the begining
         :param temp0: temperature of the begining
         :param time1: time of the end
         :param temp1: temperature of the end
         """
+        self.temp_max = temp_max
+        self.temp_min = temp_min
         self.temp1 = temp1
         self.temp0 = temp0
         self.time1 = time1
@@ -27,12 +26,12 @@ class DifferentialRegulator:
         calculate raising time
         :return: raising time
         """
-        return self.temp1 / self.derivative()
+        return (self.temp_max - self.temp_min) * self.derivative()
 
     def regulate(self):
         """
-        the raising time should be bigget tha the dt bu the ten times
-        it defines by the COEFFICIENT from the deffintions
+        the raising time should be bigget tha the dt by the ten times
+        it defines by the COEFFICIENT from the defintions
         :return: the 0 if everything is OK, 1 if refrigerator must be switched on
         """
         if self.raising_time() < self.COEFFICIENT * (self.time1 - self.time0):
@@ -42,7 +41,7 @@ class DifferentialRegulator:
 
 
 if __name__ == "__main__":
-    regulator = DifferentialRegulator()
+    regulator = DifferentialRegulator(100, 90)
 
     with open("input.txt", "r") as fo:
         lines = fo.readlines()[1:]
